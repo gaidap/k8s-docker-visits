63023eda9af977f001448c83505830fdf44dd69f
gaidap <pc.gaida@gmail.com>
2021-03-13 16:15:38 +0100

    set up little example node app

---
+++ docker/visits/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const redis = require('redis');
+
+const app = express();
+const client = redis.createClient();
+client.set('visits', 0);
+
+app.get('/', (_reqquest, response) => {
+  client.get('visits', (_error, visits) => {
+    response.send(`Number of visits is ${visits}`);
+    client.set('visits', parseInt(visits) + 1);
+  });
+});
+
+const port = 8080;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
